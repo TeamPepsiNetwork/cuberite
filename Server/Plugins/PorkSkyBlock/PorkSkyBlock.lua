a19053b027cb8b99fe99833e7f40de48dda8b1eb
DaMatrix <joey.rabil@gmail.com>
2019-03-31 15:20:17 +0200

    rename skyblock plugin

--- Server/Plugins/PorkSkyBlock/PorkSkyBlock.lua
+++ Server/Plugins/PorkSkyBlock/PorkSkyBlock.lua
@@ -1,21 +1,28 @@
--- DaPorkchop_'s anarchy SkyBlock plugin
--- Basically just a modded version of the original SkyBlock plugin for Cuberite
--- yeet
+-- DaPorkchop_'s SkyBlock plugin
+--
+-- much better than the original SkyBlock plugin for Cuberite, i made it fancier. e.g.:
+-- - player data (including progress) is stored in an SQLite database rather than individual files
+-- - challenges are defined using a much more powerful JSON-based system
+-- - built-in support for the nether
+-- - toggleable anarchy mode
 
-NAME = "AnarchySkyBlock"
+NAME = "PorkSkyBlock"
 VERSION_NUMBER = 1
 VERSION = "v0.0.1-SNAPSHOT"
 
-PLUGIN = nil
-LOCAL_FOLDER = nil
-CONFIG_FILE = nil
+PLUGIN = nil       -- plugin instance
+LOCAL_FOLDER = nil -- plugin folder
+CONFIG_FILE = nil  -- config file path
 
-WORLD_NAME = nil
-NETHER_NAME = nil
-SPAWN_RADIUS = -1
+WORLD_NAME = nil   -- name of the default world
+NETHER_NAME = nil  -- name of the nether world
+SPAWN_RADIUS = -1  -- random spawn radius
+INSTANCE_NAME = "" -- name of this server
 
-WORLD = nil
-NETHER = nil
+WORLD = nil        -- default world instance
+NETHER = nil       -- nether world instance
+
+ANARCHY = true     -- whether or not this is anarchy skyblock. if false, then this will be normal skyblock
 
 function Initialize(Plugin)
     LOG("Loading " .. NAME .. " " .. VERSION .. " (version id " .. VERSION_NUMBER .. ")")
@@ -62,8 +69,8 @@ function Initialize(Plugin)
     cPluginManager:AddHook(cPluginManager.HOOK_ENTITY_CHANGING_WORLD, OnEntityChangingWorld);
 
     -- Command Bindings
-    cPluginManager:BindCommand("/challenge", "core.build", CommandChallenge, " - View SkyBlock challenges")
-    cPluginManager:BindCommand("/start", "core.build", CommandStart, " - Get starter items for SkyBlock")
+    cPluginManager:BindCommand("/challenge", "core.build", CommandChallenge, " - View " .. INSTANCE_NAME .. " challenges")
+    cPluginManager:BindCommand("/start", "core.build", CommandStart, " - Get starter items for " .. INSTANCE_NAME)
 
     LOG(NAME .. " " .. VERSION .. " loaded successfully!")
 
@@ -88,20 +95,25 @@ function LoadConfiguration()
     configIni:DeleteHeaderComments()
     configIni:DeleteKeyComments("Worlds")
     configIni:DeleteKeyComments("General")
+    configIni:DeleteKeyComments("Anarchy")
     configIni:AddHeaderComment("Configuration file for " .. NAME)
     configIni:AddHeaderComment("Made by DaPorkchop_ for the Team Pepsi Server Network")
     configIni:AddHeaderComment("https://daporkchop.net")
     configIni:AddHeaderComment("https://pepsi.team")
     configIni:AddKeyComment("Worlds", "\"World_name\" is the name of the world that will be used as the SkyBlock overworld")
     configIni:AddKeyComment("Worlds", "\"Nether_name\" is the name of the world that will be used as the SkyBlock overworld")
-    configIni:AddKeyComment("General", "\"Spawn_radius\" is the radius (in chunks) of the spawn area")
+    configIni:AddKeyComment("General", "\"Anarchy_mode\" toggles whether or not this is normal SkyBlock or anarchy SkyBlock")
+    configIni:AddKeyComment("General", "\"Name\" is the name of this server instance")
     configIni:AddKeyComment("General", "\"Start_cooldown\" number of ticks between a player being allowed to start (i.e. obtain their starter items)")
+    configIni:AddKeyComment("Anarchy", "\"Spawn_radius\" is the radius (in chunks) of the spawn area")
 
     -- read values from config
-    WORLD_NAME = configIni:GetValueSet("Worlds", "World_name", "anarchyskyblock")
-    NETHER_NAME = configIni:GetValueSet("Worlds", "Nether_name", "anarchyskyblock_nether")
-    SPAWN_RADIUS = configIni:GetValueSetI("General", "Spawn_radius", 8)
+    ANARCHY = configIni:GetValueSetB("General", "Anarchy_mode", false)
+    INSTANCE_NAME = configIni:GetValueSet("General", "Name", "SkyBlock")
+    WORLD_NAME = configIni:GetValueSet("Worlds", "World_name", ANARCHY and "anarchyskyblock" or "skyblock")
+    NETHER_NAME = configIni:GetValueSet("Worlds", "Nether_name", ANARCHY and "anarchyskyblock_nether" or "skyblock_nether")
     START_COOLDOWN = configIni:GetValueSetI("General", "Start_cooldown", 1728000) -- default 1 day
+    SPAWN_RADIUS = configIni:GetValueSetI("Anarchy", "Spawn_radius", 8)
 
     -- sanity check values
     if (SPAWN_RADIUS < 1) then
@@ -112,7 +124,7 @@ function LoadConfiguration()
 
     SPAWN_RADIUS = (SPAWN_RADIUS * 16) - 8
 
-    -- save config again
+    -- save config again in case it changed
     configIni:WriteFile(CONFIG_FILE)
 end
 
