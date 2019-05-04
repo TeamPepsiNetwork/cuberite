64e1b6237b8bfd9d0762e708ca2e135820c463ba
DaMatrix <joey.rabil@gmail.com>
2019-05-04 18:20:30 +0200

    copypasta enchantment levels from torogadude's bedwars

--- Server/Plugins/BedWars/Hooks.lua
+++ Server/Plugins/BedWars/Hooks.lua
@@ -35,20 +35,15 @@ function ResetPlayer(a_Player)
     a_Player:SetInvulnerableTicks(40)
 
     -- default items
-    inv:SetHotbarSlot(0, cItem(E_ITEM_DIAMOND_SWORD, 1, 0, "sharpness=5;fireaspect=2;unbreaking=3;knockback=3"))
-    inv:SetHotbarSlot(1, cItem(E_ITEM_BOW, 1, 0, "punch=5;flame=1;unbreaking=3;knockback=3"))
-    inv:SetHotbarSlot(2, cItem(E_ITEM_DIAMOND_PICKAXE, 1, 0, "efficiency=7;unbreaking=3"))
-    inv:SetHotbarSlot(3, cItem(E_BLOCK_OBSIDIAN, 64, 0))
-    inv:SetHotbarSlot(8, cItem(E_ITEM_GOLDEN_APPLE, 64, E_META_GOLDEN_APPLE_ENCHANTED))
-    inv:SetInventorySlot(8, cItem(E_ITEM_ARROW, 64))
-    --inv:SetArmorSlot(0, cItem(E_ITEM_DIAMOND_HELMET, 1, 0, "unbreaking=3;blastprotection=4"))
-    --inv:SetArmorSlot(1, cItem(E_ITEM_DIAMOND_CHESTPLATE, 1, 0, "unbreaking=3;blastprotection=4"))
-    --inv:SetArmorSlot(2, cItem(E_ITEM_DIAMOND_LEGGINGS, 1, 0, "unbreaking=3;blastprotection=4"))
-    --inv:SetArmorSlot(3, cItem(E_ITEM_DIAMOND_BOOTS, 1, 0, "unbreaking=3;blastprotection=4;featherfalling=4"))
-    inv:SetArmorSlot(0, cItem(E_ITEM_DIAMOND_HELMET, 1, 0, "unbreaking=3"))
-    inv:SetArmorSlot(1, cItem(E_ITEM_DIAMOND_CHESTPLATE, 1, 0, "unbreaking=3"))
-    inv:SetArmorSlot(2, cItem(E_ITEM_DIAMOND_LEGGINGS, 1, 0, "unbreaking=3"))
-    inv:SetArmorSlot(3, cItem(E_ITEM_DIAMOND_BOOTS, 1, 0, "unbreaking=3;featherfalling=4"))
+    inv:SetHotbarSlot(0, cItem(E_ITEM_DIAMOND_SWORD, 1, 0, "sharpness=5;unbreaking=3"))
+    inv:SetHotbarSlot(1, cItem(E_ITEM_DIAMOND_PICKAXE, 1, 0, "efficiency=5;unbreaking=3"))
+    inv:SetHotbarSlot(2, cItem(E_BLOCK_OBSIDIAN, 3, 0))
+    inv:SetHotbarSlot(8, cItem(E_ITEM_GOLDEN_APPLE, 16, E_META_GOLDEN_APPLE_ENCHANTED))
+
+    inv:SetArmorSlot(0, cItem(E_ITEM_DIAMOND_HELMET, 1, 0, "unbreaking=3;protection=4"))
+    inv:SetArmorSlot(1, cItem(E_ITEM_DIAMOND_CHESTPLATE, 1, 0, "unbreaking=3;blastprotection=4"))
+    inv:SetArmorSlot(2, cItem(E_ITEM_DIAMOND_LEGGINGS, 1, 0, "unbreaking=3;protection=4"))
+    inv:SetArmorSlot(3, cItem(E_ITEM_DIAMOND_BOOTS, 1, 0, "unbreaking=3;protection=4"))
     local colors = {
         E_META_WOOL_WHITE,
         E_META_WOOL_LIGHTBLUE,
@@ -56,7 +51,7 @@ function ResetPlayer(a_Player)
         E_META_WOOL_BLUE,
         E_META_WOOL_RED
     }
-    for i = 4, 40 - 1 do
+    for i = 3, 40 - 1 do
         if (inv:GetSlot(i):IsEmpty()) then
             inv:SetSlot(i, cItem(E_ITEM_BED, 1, colors[math.random(1, #colors)]))
         end
